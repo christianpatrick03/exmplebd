8f8805ff9a5f653a1176e5a1ccd2b32898960677
souann <christianpatrick0306@gmail.com>
2024-03-11 16:12:09 +0100

    commit

--- index1.php
+++ index1.php
@@ -1,3 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>database</title>
+</head>
+<body>
 <?php
 if (getenv('JAWSDB_URL') !== false){
 $dbparts = parse_url(getenv('JAWSDB_URL'));
@@ -14,3 +22,5 @@ $database = 'test';
 }
 
 ?>
+</body>
+</html>
